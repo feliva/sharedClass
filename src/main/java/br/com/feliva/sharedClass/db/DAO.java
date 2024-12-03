a2d3da0e15e8ef3430f2f290dfdacaeb030abba6
darlan <darlan.felisberto@iffarroupilha.edu.br>
2024-12-02 23:39:08 -0300

    template de para ser seguido nos cadrastros

--- src/main/java/br/com/feliva/sharedClass/db/DAO.java
+++ src/main/java/br/com/feliva/sharedClass/db/DAO.java
@@ -2,8 +2,6 @@ package br.com.feliva.sharedClass.db;
 
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import jakarta.inject.Inject;
@@ -14,90 +12,67 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
 import jakarta.validation.Validator;
-import org.hibernate.Session;
 
-public abstract class DAO <T extends Model<?>> implements Serializable{
+public abstract class DAO <M extends Model<?>> implements Serializable{
 
 	private static final long serialVersionUID = 22021991L;
 
 	@Inject protected EntityManager em;
 	@Inject protected Validator validador;
 	
-	
-	public Set<ConstraintViolation<Object>> validate( T isValid){
+	public Set<ConstraintViolation<Object>> validate( M isValid){
 		return this.validador.validate(isValid);
 	} 
 	
-	public boolean validateAll(@Valid T isValid) throws ConstraintViolationException{
+	public boolean validateAll(@Valid M isValid) throws ConstraintViolationException{
 		return true;
 	} 
 
     @SuppressWarnings("unchecked")
-    public T findById(Integer id){
-        return  (T) em.find(
-                (Class<?>)((ParameterizedType)((Class) getClass().getGenericSuperclass()).getGenericSuperclass()).getActualTypeArguments()[0], id );
+    public M findById(Integer id){
+        return  (M) em.find(
+                (Class<?>)((ParameterizedType)((Class<?>) getClass().getGenericSuperclass()).getGenericSuperclass()).getActualTypeArguments()[0], id );
     }
 
     @SuppressWarnings("unchecked")
-    public T findById(Model entity){
-        return  (T) em.find( entity.getClass(), entity.getMMId() );
+    public M findById(Model<?> entity){
+        return  (M) em.find( entity.getClass(), entity.getMMId() );
     }
 
     @SuppressWarnings("unchecked")
-    public T findById(String id){
-        return  (T) em.find(
-                (Class<?>)((ParameterizedType)((Class) getClass().getGenericSuperclass()).getGenericSuperclass()).getActualTypeArguments()[0], id );
+    public M findById(String id){
+        return  (M) em.find(
+                (Class<?>)((ParameterizedType)((Class<?>) getClass().getGenericSuperclass()).getGenericSuperclass()).getActualTypeArguments()[0], id );
     }
-//
-//    public List<T> listAll(){
-//        this.em.getCriteriaBuilder().createQuery(this.getClass().getGenericSuperclass().getClass()).from;
-//    }
 
-    public T persist (T entity) throws RollbackException{
+    public <T> void persist (T entity) throws RollbackException{
     	em.persist( entity );
-        return entity;//remover
-    }
-
-    public void refresh(T entity) {
-        em.refresh( entity );
     }
     
-    public T merge (T entity) throws RollbackException{
-        return em.merge( entity );
+    public <T> void merge (T entity) throws RollbackException{
+        em.merge(entity);
     }
     
-    public void remove (T entity) throws RollbackException{
+    public void remove (Model<?> entity) throws RollbackException{
 		if(!entity.isNovo()) {
 			em.remove(em.contains(entity) ? entity : em.merge(entity));
 		} else {
-			// TODO remover este else
 			System.out.println("é novo, method->remove");
 		}
     }
-    
-    public void detach(T entity) throws RollbackException{
-    	this.em.detach(entity);
-    }
-    
+
     @Transactional
-    public T persistT (T entity) throws RollbackException{
-    	em.persist( entity );
-        return entity;//remover
+    public void persistT (M entity) throws RollbackException{
+    	this.persist(entity);
     }
 
 	@Transactional
-    public T mergeT (T entity) throws RollbackException{
-        return em.merge( entity );
+    public <T> void mergeT(T entity) throws RollbackException{
+        this.merge(entity);
     }
     
     @Transactional
-    public void removeT (T entity)throws RollbackException{
-		if(!entity.isNovo()) {
-			this.em.remove(em.contains(entity) ? entity : em.merge(entity));
-		} else {
-			// TODO remover este else
-			System.out.println("é novo, method->removeT");
-		}
+    public void removeT (M entity)throws RollbackException{
+		this.remove(entity);
     }
-
 }
