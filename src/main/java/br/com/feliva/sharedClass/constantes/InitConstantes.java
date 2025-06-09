dccc058ebf52f815bd085b71dd9f5b7108035a06
darlanfelisberto <darlanfelisberto@gmail.com>
2025-06-09 16:13:50 -0300

    correções

--- src/main/java/br/com/feliva/sharedClass/constantes/InitConstantes.java
+++ src/main/java/br/com/feliva/sharedClass/constantes/InitConstantes.java
@@ -3,14 +3,23 @@ package br.com.feliva.sharedClass.constantes;
 import java.io.*;
 import java.util.Properties;
 
-public class InitConstantes2 {
+public class InitConstantes {
 
 	private static final Properties config = new Properties();
 
 	static {
+		//OBS:
+		//este aquivo de configuração pertence ao projeto sifw, mas ele contém paramentros utilizados para
+		//a autoconfiguração do sifw e de outros sistemas, que utilizam a mesma base, por isso ele é apenas copiado para essa lib,sharedClass
 		String nameFileContantes = "config_sifw.properties";
 		try {
-			InputStream configfile = InitConstantes2.class.getClassLoader().getResourceAsStream(nameFileContantes);
+			//Tenta pegar o arquivo do projeto principal(sifw,auth,..)
+			InputStream configfile = Thread.currentThread().getContextClassLoader().getResourceAsStream(nameFileContantes);
+			// se não existir
+			if (configfile == null) {
+				// pega o da shareClass
+				configfile = InitConstantes.class.getClassLoader().getResourceAsStream(nameFileContantes);
+			}
 	        if (configfile != null) {
 				config.load(configfile);
 	        }
